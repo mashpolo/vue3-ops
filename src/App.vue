5de6b4ce84a368a495ae8a002802c08fa71e9f72
Frank Luo <luluo.polo1@gmail.com>
2022-03-03 14:51:11 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+  import Child from './views/Child.vue'
+  import { onMounted, ref } from 'vue'
+
+  defineProps({
+    msg: {
+      type: String,
+      default: ()=>{return "hello, world"}
+    }
+  })
+
+  const handle = (params: string) => {
+    console.log(params)
+  }
+  const testChild = () =>{
+    let a: Object = {'a': 'b'}
+    console.log(a)
+  }
+  testChild()
+  const child = ref()
+  onMounted (() => {
+    console.log(child.value.testChild())
+  })
+</script>
+
+<template>
+  <Child @on-change="handle" ref="child"></Child>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/Child.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+  default: ()=>{return "hello, child"}
+})
+props: []
+const msg = ref('hello')
+const testChild = ()=> {
+  // this.msg = "hey, call ok"
+  msg.value = 'iiii'
+  console.log('hello, child ref child')
+  return 0
+}
+
+const emit = defineEmits(['on-change'])
+const handle = () => {
+  emit('on-change', 'child methods')
+}
+defineExpose({
+  testChild
+})
+
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <button @click="handle">click</button>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
